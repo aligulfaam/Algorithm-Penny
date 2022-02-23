3c98e30484fa4aafb145f6b9e2f2f6ba7b96f177
aligulfaamstudent <90401861+aligulfaamstudent@users.noreply.github.com>
2022-02-23 13:23:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ var pennies, dollars, quarters, dimes, nickels, pennies;
 function calculate(){
    pennies = document.getElementById("pennyInput").value;
 
-    dollars = (pennies / 100).toFixed(0);
+    dollars = Math.floor((pennies / 100));
     document.getElementById("dollarsNeeded").innerHTML = dollars;
     pennies %= 100;
 
-    quarters = (pennies / 25).toFixed(0);
+    quarters = Math.floor((pennies / 25));
     document.getElementById("quartersNeeded").innerHTML = quarters;
     pennies %= 25;
 
-    dimes = (pennies / 10).toFixed(0);
+    dimes = Math.floor((pennies / 10));
     document.getElementById("dimesNeeded").innerHTML = dimes;
     pennies %= 10;
 
-    nickels = (pennies / 5).toFixed(0);
+    nickels = Math.floor((pennies / 5));
     document.getElementById("nickelsNeeded").innerHTML = nickels;
     pennies %= 5;
 
